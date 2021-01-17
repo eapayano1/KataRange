ccd8e77df3864ce595d45bd058cd291f07ae2008
Emiko47 <emilpayano70@gmail.com>
2021-01-17 11:44:45 -0400

    Borrar segundo kata

--- test/katarange/core_test.clj
+++ test/katarange/core_test.clj
@@ -2,6 +2,4 @@
   (:require [clojure.test :refer :all]
             [katarange.core :refer :all]))
 
-(deftest a-test
-  (testing "Testing-include?"
-    (is (= (includes? "[2,5]" "{2,3,4,5}")true))))
+
